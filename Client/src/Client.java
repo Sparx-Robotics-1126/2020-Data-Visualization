51b4ea23346d825671f809ee8c5ca4821e1f77c7
Nathan <nxc5929@rit.edu>
2019-11-25 19:16:27 -0500

    Working Stream

--- Client/src/Client.java
+++ Client/src/Client.java
@@ -1,24 +1,38 @@
+package frc.robot;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.InetSocketAddress;
 import java.net.Socket;
 
-public abstract class Client {
+public abstract class Client extends Thread {
+
+    private final String HOSTNAME = "10.11.26.190";
+    private final int PORT = 8080;
 
     private String name;
+    private Socket socket;
     private PrintWriter writer;
     private long startTime;
 
     public Client(String name) throws IOException {
         this.name = name;
-        writer = new PrintWriter(new Socket("localhost", 8080).getOutputStream());
+        Socket socket = new Socket();
+        socket.connect(new InetSocketAddress(HOSTNAME, PORT), 1000);
+        writer = new PrintWriter(socket.getOutputStream());
         startTime = System.currentTimeMillis();
     }
 
+    @Override
     public void run() {
-        long time = System.currentTimeMillis() - startTime;
-        String json = "{\"name\": \"" + name + "\", \"timestamp\": " + time  + ", \"x\": " + getXValue() + "}";
-        writer.print(json);
-        writer.flush();
+        while(!socket.isClosed()){
+            long time = System.currentTimeMillis() - startTime;
+            String json = "{\"name\": \"" + name + "\", \"timestamp\": " + time + ", \"x\": " + getXValue() + "}";
+            writer.print(json);
+            writer.flush();
+            try {Thread.sleep(100);} catch (InterruptedException e) {}
+        }
+        try { socket.close(); } catch (IOException e) {e.printStackTrace();}
     }
 
     public abstract double getXValue();
--- Data-Visualization/src/app/app.component.ts
+++ Data-Visualization/src/app/app.component.ts
@@ -8,15 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent {
   title = 'Data-Visualization';
   socket = new WebSocket("ws://localhost:8081");
-  data = [{name: "Test", timestamp: 1, x: 1}];
+  data = {name: "Test", timestamp: 1, x: 1};
 
   ngOnInit(){
     var self = this;
     this.socket.onmessage = function(result){
       var json = JSON.parse(result.data);
       if(json.name){
-        console.log(json);
-        self.data.push(json);
+        self.data = json;
       }
     }
   }
